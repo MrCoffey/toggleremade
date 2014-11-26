996803fab201695f95e94eb79bc1217a97abf82c
Jorge Madrid <jorjilio@hotmail.com>
2014-11-25 22:36:21 -0500

    date helpers

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ActiveRecord::Base
+	#Listando de menor a mayor
+	default_scope { order(created_at: :desc) }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Tag.delete_all
+
+Tag.create(title: "Una actividad", tag_id: 1)
